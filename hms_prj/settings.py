4467249612136d5d6f190c0e3389080c18bff263
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-17 12:50:34 -0800

    installed jazzmin admin dashboard to ovverdie the default django admin dahsboard

--- hms_prj/settings.py
+++ hms_prj/settings.py
@@ -33,18 +33,22 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [ 
+    'jazzmin',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     
     
     'base',
     'doctor',
     'patient',
     'userauths',
+ 
     
 ]
 
