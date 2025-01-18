127aa343d597fcddf67b7df1c8dc7adfa5946771
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-17 22:54:09 -0800

    added all the admins model

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from doctor import models
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'specialization', 'qualifications', 'years_of_experience']
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'appointment', 'type', 'seen', 'date']
+
+
+admin.site.register(models.Doctor, DoctorAdmin)
+admin.site.register(models.Notification, NotificationAdmin)
