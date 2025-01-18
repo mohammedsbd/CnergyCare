3290b16310babbd2c69997a516fd65ecebfa6177
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-18 05:50:14 -0800

    added login function

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate ,login, logout
 from userauths import forms as userauths_forms
+from doctor import models as doctor_models
+from patient import models as patient_models
 
 # Create your views here.
 
@@ -9,26 +11,50 @@ def Register_view(request):
     if request.user.is_authenticated:
         messages.success(request,"You are already logged in")
         return redirect('/')
-    form=userauths_forms.UserRegistrationForm(request.POST or None)
+    
+    
+    if request.method == 'POST':
+        form=userauths_forms.UserRegistrationForm(request.POST or None)
     
     if form.is_valid():
        user= form.save()
        full_name=form.cleaned_data.get("full_name")
        email = form.cleaned_data.get("email")
-       password=form.cleaned_data.get("password")
+       password1=form.cleaned_data.get("password1")
        user_type=form.cleaned_data.get("user_type")
-   
+       
+       
+       
        
     #    to immidetly login the user without taking time
-       user_=authenticate(email=email, password=password)
-       login(request, user_)
+       user=authenticate(request,email=email, password=password1)
+       print("user type =======" ,user_type)
+       if user is not None:
+           login(request,user)
+           
+           
+           if user_type=="Doctor":
+               doctor_models.Doctor.objects.create(user=user, full_name=full_name)
+           else:
+               patient_models.Patient.objects.create(user=user, full_name=full_name, email=email)
+   
     
-       messages.success(request,"Account Created Successfully")
-       return redirect('/')
+           messages.success(request,"Account Created Successfully")
+           return redirect('/')
     
+    else:
+            messages.error(request,"Authentication Failed, please try again")
         
-        
+    
     context={
         "form": form
     }
     return render(request,"userauths/sign-up.html",context)
+
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.success(request,"You are already logged in")
+        return redirect('/')
+    form=userauths_forms.UserRegistrationForm(request.POST or None)
