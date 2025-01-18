1ab5ce06d7871ebd6e6593d07abe77c2f76a6feb
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-17 20:35:22 -0800

    added the userauths model

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+# creating a model for email ovveriding default django user model
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username=models.CharField(max_length=100,null=True, blank=True)
+    
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=["username"]
+    
+    def __str__(self):
+        return self.username
+    
+    # this means the function can accept arguments and keywoard arguments
+    def save(self, *args, **kwargs):
+        email_username, mobile=self.email_split("@")
+    if self.username =="" or self.username ==None:
+        self.username = email_username
+    
+    super(User, self).save(*args, **kwargs) 
