38b28c8f8f782a9fdc0eec71ae8a01d2f6e8ee66
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-18 09:27:45 -0800

    finished the sing in and sign up page templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from base import models as base_models
 
 def index(request):
-    return render(request, 'base/index.html')
+    services=base_models.Service.objects.all()
+    
+    context={
+        "services": services
+    }
+    return render(request, 'base/index.html',context)
 
 def service_detail(request,service_id):
+      services=base_models.Service.objects.get()
     return render(request, 'base/service_detail.html')
