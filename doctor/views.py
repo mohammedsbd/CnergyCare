8442e175fd709edc2465e36cfac61520613749f9
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-19 06:26:44 -0800

    added doctors dashboard function

--- doctor/views.py
+++ doctor/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from doctor import models as doctor_models
+from base import models as base_models
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(doctor=doctor)
+    notifications = doctor_models.Notification.objects.filter(doctor=doctor)
+
+    context = {
+        "appointments": appointments,
+        "notifications": notifications,
+    }
+
+    return render(request, "doctor/dashboard.html", context)
+
