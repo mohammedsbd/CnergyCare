21d4ff52ad4c43f45a3917f216c05dd5e34ed23e
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-19 08:25:19 -0800

    finshed appointments page

--- doctor/urls.py
+++ doctor/urls.py
@@ -5,5 +5,7 @@ app_name="doctor"
 
 urlpatterns =[
     path("", views.dashboard, name="dashboard"),
+    path("appointments/", views.appointments, name="appointments"),
+    path("appointments/<appointment_id>", views.appointment_detail, name="appointment_detail"),
 
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -16,3 +16,31 @@ def dashboard(request):
 
     return render(request, "doctor/dashboard.html", context)
 
+@login_required
+def appointments(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(doctor=doctor)
+
+    context = {
+        "appointments": appointments,
+    }
+
+    return render(request, "doctor/appointments.html", context)
+
+@login_required
+def appointment_detail(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+    
+    medical_records = base_models.MedicalRecord.objects.filter(appointment=appointment)
+    lab_tests = base_models.LabTest.objects.filter(appointment=appointment)
+    prescriptions = base_models.Prescription.objects.filter(appointment=appointment)
+
+    context = {
+        "appointment": appointment,
+        "medical_records": medical_records,
+        "lab_tests": lab_tests,
+        "prescriptions": prescriptions,
+    }
+
+    return render(request, "doctor/appointment_detail.html", context)
