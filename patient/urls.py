53a1158621f3ab019b5e77504005744a60cc50fc
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-20 03:16:34 -0800

    pateint appointment function finished

--- patient/urls.py
+++ patient/urls.py
@@ -5,6 +5,8 @@ app_name="patient"
 
 urlpatterns =[
     path("", views.dashboard, name="dashboard"),
+    path("appointments", views.appointments, name="appointments"),
+    path("appointment_detail/<appointment_id>", views.appointment_detail, name="appointment_detail"),
    
 
 ]
