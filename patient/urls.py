fae8608c07c24d4051359c7ac762d1113a20a43b
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-20 04:04:58 -0800

    finshed evrything on patient payment and notifcation

--- patient/urls.py
+++ patient/urls.py
@@ -11,6 +11,10 @@ urlpatterns =[
      path("cancel_appointment/<appointment_id>", views.cancel_appointment, name="cancel_appointment"),
     path("activate_appointment/<appointment_id>", views.activate_appointment, name="activate_appointment"),
     path("complete_appointment/<appointment_id>", views.complete_appointment, name="complete_appointment"),
+    
+    path("payment/",views.payments, name="payments"),
+    path("notifications/",views.notifications, name="notifications"),
+    path("mark_noti_seen/<id>",views.mark_noti_seen, name="mark_noti_seen"),
    
 
 ]
