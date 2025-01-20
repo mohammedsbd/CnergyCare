4fea885d1e56858728d3623dcd53639176aa4590
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-20 02:27:47 -0800

    added the pateind dashboard function

--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.db import models
+
+from base import models as base_models
+from patient import models as patient_models
+
+
+
+@login_required
+def dashboard(request):
+    patient = patient_models.Patient.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(patient=patient)
+    notifications = patient_models.Notification.objects.filter(patient=patient, seen=False)
+    total_spent = base_models.Billing.objects.filter(patient=patient).aggregate(total_spent = models.Sum("total"))['total_spent']
+    
+    context = {
+        'appointments': appointments,
+        'notifications': notifications,
+        'total_spent': total_spent,
+    }
+
+    return render(request, "patient/dashboard.html", context)
