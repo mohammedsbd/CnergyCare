83873db228013f628c5819fa2fdd5da50f5da07f
mohammedsbd <Mohammed1112ok@gmail.com>
2025-01-20 03:35:42 -0800

    added the url forpatien appointment

--- patient/urls.py
+++ patient/urls.py
@@ -7,6 +7,10 @@ urlpatterns =[
     path("", views.dashboard, name="dashboard"),
     path("appointments", views.appointments, name="appointments"),
     path("appointment_detail/<appointment_id>", views.appointment_detail, name="appointment_detail"),
+    
+     path("cancel_appointment/<appointment_id>", views.cancel_appointment, name="cancel_appointment"),
+    path("activate_appointment/<appointment_id>", views.activate_appointment, name="activate_appointment"),
+    path("complete_appointment/<appointment_id>", views.complete_appointment, name="complete_appointment"),
    
 
 ]
